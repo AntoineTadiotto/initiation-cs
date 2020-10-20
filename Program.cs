28a73d89932b852565688bb75d7756373c1e3cdf
antoine_tadiotto <antoine.tadiotto@gmail.com>
2020-10-20 14:18:42 +0200

    dictionnaire

--- Program.cs
+++ Program.cs
@@ -10,13 +10,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var fruits = new List<string> { "pomme", "poire", "orange", "banane", "kiwi" };
-            fruits.Remove("banane");
-            foreach (var fruit in fruits)
+            Dictionary<string, int> membre = new Dictionary<string, int>();
+            membre.Add("hubert laurent", 20);
+            membre.Add("patrick paul", 24);
+            membre.Add("john john", 40);
+            membre.Add("aaaaa aaaaaa", 20);
+            membre.Add("a p", 20);
+            membre.Add("p o", 43);
+            Console.WriteLine(membre["hubert laurent"]);
+            membre.Remove("john john");
+            membre.Remove("p o");
+            foreach (KeyValuePair<string, int> entry in membre)
             {
-                Console.WriteLine(fruit);
+                Console.WriteLine(entry.Key + " a " + entry.Value + " ans ");
             }
-            
+
+
             Console.ReadLine();
         }
     }
