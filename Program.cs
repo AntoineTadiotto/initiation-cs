f63cff8ca7e1e8d888dd07ad14276bf3a3340d6a
antoine_tadiotto <antoine.tadiotto@gmail.com>
2020-10-20 13:45:52 +0200

    tableau

--- Program.cs
+++ Program.cs
@@ -10,29 +10,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            double a = 12 + 5 * 12.5 - 1253.68;
-            Console.WriteLine(a);
-
-            double b = 0;
-            double[] val = new double[5] { 1.0, 5.5, 9.9, 2.8, 9.6 };
-            for (int i = 0; i < val.Length; i++)
+            int[] age = { 13, 12, 70, 14, 20, 50 };
+            string[] prenom = { "antoine", "gregory", "sami", "thomas", "anthony", "baptiste" };
+            int l = age.Length;
+            for (int i = 0; i < l; i++)
             {
-                b += val[i];
+                Console.WriteLine(prenom[i] + " : " + age[i]);
             }
-
-            double moy = b / val.Length;
-            Console.WriteLine(moy);
-
-            Console.WriteLine("rayon de cercle");
-            double rayon = double.Parse(Console.ReadLine());
-            double per = 2 * Math.PI * rayon;
-            double s = Math.PI * rayon * rayon;
-            Console.WriteLine("perimetre " + per);
-            Console.WriteLine("surface " + s);
-
-            string chaine = "zenat";
-
-
             Console.ReadLine();
         }
     }
